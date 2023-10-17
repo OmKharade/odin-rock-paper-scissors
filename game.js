ed3683a5f65391c43061dfdf1b85aff88f2242ce
OmKharade <133861833+OmKharade@users.noreply.github.com>
2023-10-16 22:57:33 -0400

    Added UI - !removed retro play button

--- game.js
+++ game.js
@@ -8,37 +8,61 @@ function getComputerChoice(){
         return "scissors";
 }
 
-function game(){
-    let playerScore = 0
-    let computerScore = 0
-    for(let i=0;i<5;i++){
-        playerChoice = prompt("Rock, Paper, Scissors, SHOOT!").toLowerCase();
-        computerChoice = getComputerChoice();
-        console.log(computerChoice);
-        if (playerChoice==computerChoice){
-            console.log("Tied! Both chose",(playerChoice),"!");
-            playerScore += 1;
-            computerScore += 1;
-        }
-        else if((playerChoice=="rock"&&computerChoice=="scissors")||
-                (playerChoice=="paper"&&computerChoice=="rock")   ||
-                (playerChoice=="scissor"&&computerChoice=="paper")){
-                console.log("Round Won!",(playerChoice),"beats",(computerChoice));
-                playerScore += 1;
-        }
-        else{
-            console.log("Round Lost :(",(computerChoice),"beats",(playerChoice));
-            computerScore += 1;
-        }
-        console.log("SCORE - Player :",(playerScore),"| Computer ",(computerScore));
+let playerScore = 0
+let computerScore = 0
+
+function playRound(playerChoice){
+    computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    if (playerChoice==computerChoice){
+        document.getElementById('playerScore').innerHTML = 'Score : '+(++playerScore);
+        document.getElementById('computerScore').innerHTML = 'Score : '+(++computerScore);
     }
-    if(computerScore==playerScore)
-        console.log("Game TIED");
-    else if(computerScore>playerScore)
-        console.log("You Lost, Try Again!");
-    else
-        console.log("You WIN!");
+    else if((playerChoice=="rock"&& computerChoice=="scissors")||
+            (playerChoice=="paper"&& computerChoice=="rock")   ||
+            (playerChoice=="scissor"&& computerChoice=="paper")){
+            console.log("Round Won!",(playerChoice),"beats",(computerChoice));
+            document.getElementById('playerScore').innerHTML = 'Score : '+(++playerScore);
+    }
+    else{
+        console.log("Round Lost :(",(computerChoice),"beats",(playerChoice));
+        document.getElementById('computerScore').innerHTML = 'Score : '+(++computerScore);
+    }
+    console.log("SCORE - Player :",(playerScore),"| Computer ",(computerScore));
+    if(computerScore==5 || playerScore==5){
+        document.getElementById('game').style.display = "none"
+        if(computerScore==playerScore)
+            document.getElementById('gameTied').style.display = "inline"
+        else if(computerScore>playerScore)
+            document.getElementById('computerWin').style.display = "inline"
+        else
+            document.getElementById('playerWin').style.display = "inline"
+    }
 }
 
+let start = document.getElementById('start')
 let startBtn = document.querySelector('#startBtn')
-startBtn.addEventListener('click',game)
+
+startBtn.addEventListener('click',function(){
+    start.style.display = "none"
+});
+document.addEventListener('click',gameChoiceListener)
+function gameChoiceListener(event){
+    let choice = event.target;
+    console.log(choice);
+         if((choice.classList.contains('gameChoice') || 
+             choice.classList.contains('rockTxt') ||
+             choice.classList.contains('rockImg'))&& 
+             choice.id === ('rock'))
+             playRound("rock");
+    else if((choice.classList.contains('gameChoice') || 
+             choice.classList.contains('paperTxt') ||
+             choice.classList.contains('paperImg'))&& 
+             choice.id === ('paper'))
+             playRound("paper");
+    else if((choice.classList.contains('gameChoice') || 
+             choice.classList.contains('scissorTxt') ||
+             choice.classList.contains('scissorImg'))&& 
+             choice.id === ('scissor'))
+             playRound("scissor");
+}
